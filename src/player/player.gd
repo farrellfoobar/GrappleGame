e6222f238c4cc85edf0a8aa57717615a28265bad
Ava Farrell <avafarrell415@gmail.com>
2024-12-05 00:03:28 -0800

    working grapple with third person character

--- src/player/player.gd
+++ src/player/player.gd
@@ -9,7 +9,7 @@ const ROTATION_INTERPOLATE_SPEED = 10
 
 const MIN_AIRBORNE_TIME = 0.1
 const MIN_AIR_TIME_FOR_LANDING_ANIMATION = 0.5
-const JUMP_SPEED = 5
+const JUMP_SPEED = 8
 
 
 @onready var initial_position = transform.origin
@@ -44,14 +44,21 @@ func _ready():
 
 
 func _physics_process(delta: float):
+	# If we're below -40, respawn (teleport to the initial position).
+	if transform.origin.y < -40:
+		transform.origin = initial_position
+		
 	apply_input(delta)
 
 func apply_input(delta: float):
 	rotate_player_from_input(delta)
 	do_jump_logic(delta)
 	apply_input_to_character(delta)
+	#print("v1: ", velocity)
 	animate_player()
-	handle_shoot()
+	#handle_shoot()
+	impart_velocity_from_grapple(delta)
+	#print("v2: ", velocity)
 	move_and_slide()
 
 
@@ -77,25 +84,25 @@ func do_jump_logic(delta: float):
 var root_motion: Transform3D       = Transform3D()
 var input_motion: Vector2          = Vector2()
 var animation_tree_motion: Vector2 = Vector2()
-var orientation: Transform3D = Transform3D()
+var orientation: Transform3D = Transform3D() # not just used here, side affect in other methods
 func apply_input_to_character(delta: float):
 	input_motion = input_motion.lerp(player_input.motion, MOTION_INTERPOLATE_SPEED * delta)
 	update_animation_tree_root_motion(input_motion)
 
+	velocity += gravity * delta
 	if is_on_floor():
 		root_motion = Transform3D(animation_tree.get_root_motion_rotation(), animation_tree.get_root_motion_position())
-
-	orientation *= root_motion
-
-	var horizontal_velocity: Vector3 = orientation.origin / delta
-	velocity.x = horizontal_velocity.x
-	velocity += gravity * delta
-	velocity.z = horizontal_velocity.z
+		orientation *= root_motion
+		var horizontal_velocity: Vector3 = orientation.origin / delta
+		velocity.x = horizontal_velocity.x
+		velocity.z = horizontal_velocity.z
 
 	orientation.origin = Vector3() # Clear accumulated root motion displacement (was applied to speed).
 	orientation = orientation.orthonormalized() # Orthonormalize orientation.
 
 	player_model.global_transform.basis = orientation.basis
+
+
 
 	
 func animate_player():
@@ -116,11 +123,6 @@ func animate_player():
 
 func is_landing() -> bool:
 	return is_on_floor() and airborne_time > MIN_AIR_TIME_FOR_LANDING_ANIMATION
-
-func handle_shoot():
-	if player_input.aiming:
-		if player_input.shooting and fire_cooldown.time_left == 0:
-			shoot_bullet()
 
 
 func update_animation_tree_root_motion(input: Vector2):
@@ -151,6 +153,7 @@ func rotate_character_with_locked_camera(delta: float):
 func get_basis_for_transform(from: Quaternion, to: Quaternion, delta: float) -> Basis:
 	return Basis(from.slerp(to, delta * ROTATION_INTERPOLATE_SPEED))
 
+
 func shoot_bullet():
 	var shoot_origin = shoot_from.global_transform.origin
 	var shoot_dir = (player_input.shoot_target - shoot_origin).normalized()
@@ -162,8 +165,75 @@ func shoot_bullet():
 	bullet.look_at(shoot_origin + shoot_dir, Vector3.UP)
 	bullet.add_collision_exception_with(self)
 	shoot.rpc()
+
+var is_grappled: bool          = false
+var grapple_rest_length: float = 0
+var grapple_extension: float   = 0
+var last_grapple_extension: float   = 0
+var pull_vector: Vector3
+var tension_force: Vector3
+var grapple_target: Vector3
+var mesh: ImmediateMesh = ImmediateMesh.new()
+func impart_velocity_from_grapple(delta: float) -> void:
+	if(is_grappled):
+		pull_vector = get_grapple_path().normalized()
+		last_grapple_extension = grapple_extension
+		grapple_extension = get_grapple_path().length() - grapple_rest_length
+		line(position, grapple_target,delta)
+		if(grapple_extension > 0 && grapple_extension > last_grapple_extension):
+			tension_force = get_grapple_path() * (get_gravity().length()/get_grapple_path().length())
+			tension_force = tension_force * (1 + sin(pull_vector.angle_to(get_gravity())))
+			tension_force.y *= grapple_extension
+			velocity += tension_force * delta
+
+	if Input.is_action_just_pressed("grapple"):
+		var origin = position + player_input.camera_camera.project_ray_normal(get_viewport().get_mouse_position()) 
+		var end = position + player_input.camera_camera.project_ray_normal(get_viewport().get_mouse_position()) * 1000	
+		var grapple_target_maybe = get_world_3d().direct_space_state.intersect_ray(PhysicsRayQueryParameters3D.create(origin, end))
+		if grapple_target_maybe.has("position"):
+			grapple_target = grapple_target_maybe["position"]
+			grapple_rest_length = get_grapple_path().length()
+			is_grappled = true
+
+	if Input.is_action_just_released("grapple"):
+		is_grappled = false
+		
+		
+func get_grapple_path() -> Vector3:	
+	return grapple_target-position
+
+
+func line(pos1: Vector3, pos2: Vector3, persist_ms: float = 0):
+	var mesh_instance := MeshInstance3D.new()
+	var immediate_mesh := ImmediateMesh.new()
+	var material := ORMMaterial3D.new()
+
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(pos1)
+	immediate_mesh.surface_add_vertex(pos2)
+	immediate_mesh.surface_end()
+
+	#get_tree().get_root().add_child(mesh_instance)
+
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	return await final_cleanup(mesh_instance, persist_ms)
+
+
+func final_cleanup(mesh_instance: MeshInstance3D, persist_ms: float):
+	get_tree().get_root().add_child(mesh_instance)
+	if persist_ms == 1:
+		await get_tree().physics_frame
+		mesh_instance.queue_free()
+	elif persist_ms > 0:
+		await get_tree().create_timer(persist_ms).timeout
+		mesh_instance.queue_free()
+	else:
+		return mesh_instance
 	
-	
+
 func animate(anim: int):
 	current_animation = anim
 
